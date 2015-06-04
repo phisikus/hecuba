e475f70b2327213d21a4754c457f5e2c0bd9deed
phisikus <phisikus@platinum.linux.pl>
2015-06-04 22:01:29 +0200

    fixed ObjectEntryDAO.java

--- src/main/java/pl/poznan/put/cs/dsg/srds/cassandra/dao/ObjectEntryDAO.java
+++ src/main/java/pl/poznan/put/cs/dsg/srds/cassandra/dao/ObjectEntryDAO.java
@@ -2,11 +2,14 @@ package pl.poznan.put.cs.dsg.srds.cassandra.dao;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pl.poznan.put.cs.dsg.srds.cassandra.model.ObjectEntry;
 
-public class ObjectEntryDAO extends AbstractCassandraDAO {
+public class ObjectEntryDAO extends AbstractCassandraDAO<ObjectEntry> {
 
 
     private static final Logger LOG = LoggerFactory.getLogger(ObjectEntryDAO.class);
 
-
+    public ObjectEntryDAO() {
+        super(ObjectEntry.class);
+    }
 }
