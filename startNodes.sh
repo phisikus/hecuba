7c2088da99e604c6b3c0a5c4f77e04222944cef9
phisikus <phisikus@platinum.linux.pl>
2015-06-04 18:10:15 +0200

    Clustering scripts are working

---
+++ startNodes.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ $# -lt 1 ]
+then
+	echo "use number of nodes as argument"
+	exit -1
+fi
+
+if [ $1 -lt 1 ]
+then
+	echo "use a number greater than 0"
+	exit -1
+fi
+
+numberOfNodes=$1
+for ((i=0; i<numberOfNodes; i++))
+do
+	export CASSANDRA_HOME=./nodes/node_$i/apache-cassandra/
+	screen -S cassandra_$i -dm ./nodes/node_$i/apache-cassandra/bin/cassandra -f
+	echo "Started node_$i"
+done
+echo "Done."
